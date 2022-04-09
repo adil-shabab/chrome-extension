6cfd9ce502e2213a9d0944a4145262393c11d97a
Adil Shabab <adilshabab2004@gmail.com>
2022-04-09 14:53:33 +0530

    li added in script

--- js/script.js
+++ js/script.js
@@ -3,24 +3,29 @@ const inputEl = document.getElementById('input-el')
 const saveInputBtn = document.getElementById('inp-btn')
 const saveTabBtn = document.getElementById('tab-btn')
 const deleteBtn = document.getElementById('dlt-btn')
+let ulEl = document.getElementById('ul-el')
+
+saveInputBtn.addEventListener("click", function(){
+    myTabs.push(inputEl.value)
+    inputEl.value = ""
+    renderTabs()
+})
 
 function renderTabs(){
-    let listItems = ""
+    listItems = ""
     for(let i=0; i < myTabs.length; i++){
         listItems += `
         <li>
-            <a target="_blank" href='${myTabs[i]}'>
+            <a target='_blank' href"${myTabs[i]}">
                 ${myTabs[i]}
             </a>
         </li>
         `
     }
+    ulEl.innerHTML = listItems
 }
 
-saveInputBtn.addEventListener("click", function(){
-
-})
 
 
 
-console.log(myTabs)
+// console.log(listItems)
